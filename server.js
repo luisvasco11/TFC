979904965254b4b9a223f04b1168584b59e6080d
Luis Vasco <luisvasco@MacBook-Air-de-Luis.local>
2020-03-06 19:15:27 -0500

    new changes

--- server.js
+++ server.js
@@ -7,5 +7,6 @@ app.use(express.static(__dirname+'/dist/TFC'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/TFC/index.html'));
 });
+console.log(__dirname)
 
 app.listen(process.env.PORT || 8080);
